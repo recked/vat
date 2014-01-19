9ecca736f58fb1a8b4163e735e55d5e1c2838e2a
recked <flipmodes01@gmail.com>
2014-01-19 16:50:22 -0500

    Tracking files now

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//Custom JS for project 1
+
+//jquery 
+
+$(document).ready(function () {
+    $('#good1').hide();
+    $('#good2').hide();
+    $('#good3').hide();
+    
+   // $('#good').modal();
+
+
+});
+
+//Starter code
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+
+
+
+    var data = ev.dataTransfer.getData("Text"); //data is id of image
+    var f = document.getElementById(data); // f is object of the image
+    ev.target.appendChild(f);
+
+    var what = ev.target.id; //id of the memebership boxes. 
+
+    document.getElementById("notice").innerHTML = data + " arrived at " + what;
+
+    // continue btn controls
+    if (what == 'mem1') {
+        $('#good2').hide();
+        $('#good3').hide();
+        
+        $('#good1').show();
+
+    } else if (what == 'mem2') {
+        $('#good1').hide();
+        $('#good3').hide();
+        
+        $('#good2').show();
+    } else if (what == 'mem3') {
+        $('#good2').hide();
+        $('#good1').hide();
+        
+        $('#good3').show();
+    } else {
+        $('#good1').hide();
+        $('#good2').hide();
+        $('#good3').hide();
+
+    }
+
+
+}
