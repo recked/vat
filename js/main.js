542203974471d7db865a53b41752732890579e2f
recked <flipmodes01@gmail.com>
2014-01-22 21:56:31 -0500

    More JavaScript things

--- js/main.js
+++ js/main.js
@@ -61,43 +61,63 @@ function drop(ev) {
 
 }
 
+/*allowDrop(ev);
+drag(ev);
+drop(ev);*/
 
-// Filling the modal with info for each membership
+
+// Filling the modal with info for each membership and for each coupon
 
 $("#good1").click(function (event) {
 
     $("#membership").html('Thank you for choosing the Frugal plan');
+    if ($('#mem1').find('#drag1').length == 1) { //checking if this coupon is in this membership
+        $("#amount").html('$5 off 3 months');
+        $("#total").html('$52');
+    } else if ($('#mem1').find('#drag2').length == 1) {
+        $("#amount").html('$10 off 6 months');
+        $("#total").html('$104');
+    } else if ($('#mem1').find('#drag3').length == 1) {
+        $("#amount").html('$25 off 1 year');
+        $("#total").html('$203');
+    }
 
 });
 $("#good2").click(function (event) {
 
     $("#membership").html('Thank you for choosing the Pro plan');
-
+    if ($('#mem2').find('#drag1').length == 1) {
+        $("#amount").html('$5 off 3 months');
+        $("#total").html('$112');
+    } else if ($('#mem2').find('#drag2').length == 1) {
+        $("#amount").html('$10 off 6 months');
+        $("#total").html('$224');
+    } else if ($('#mem2').find('#drag3').length == 1) {
+        $("#amount").html('$25 off 1 year');
+        $("#total").html('$443');
+    }
 
 });
-$("#good3").click(function (event) {
+$("#good3").click(function (event) { //Make these values for total something strange since the membership is free
 
     $("#membership").html('Thank you for choosing the Attonery plan');
-
-
-});
-
-// Filling the modal with info for each coupon. Check for each coupon type and store the one based on which one is in the container?
-
-$("#good1").click(function (event) {
-
-    $("#amount").html('3 months');
-
-});
-$("#good2").click(function (event) {
-
-    $("#amount").html('6 months');
-
+    if ($('#mem3').find('#drag1').length == 1) {
+        $("#amount").html('$5 off 3 months');
+        $("#total").html('$52');
+    } else if ($('#mem3').find('#drag2').length == 1) {
+        $("#amount").html('$10 off 6 months');
+        $("#total").html('$104');
+    } else if ($('#mem3').find('#drag3').length == 1) {
+        $("#amount").html('$25 off 1 year');
+        $("#total").html('$203');
+    }
 
 });
-$("#good3").click(function (event) {
 
-
-    $("#amount").html('1 year');
-
+// when the user clicks continue or clicks outside the modal, clear the page
+$('#clear').click(function (event) {
+    $('#blank').html('All gone!');
 });
+$('#good').on('hidden.bs.modal', function (e) {
+    $('#blank').html('All gone!');
+})
